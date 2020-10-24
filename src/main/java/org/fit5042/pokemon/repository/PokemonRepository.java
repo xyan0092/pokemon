bf212c637fff21ecebcda99a75dee7c2b0b4b1de
xyan0092 <xiaoxiaoyan0925@gmail.com>
2020-10-24 17:49:10 +1100

    Initial commit

---
+++ src/main/java/org/fit5042/pokemon/repository/PokemonRepository.java
@@ -0,0 +1,4 @@
+package org.fit5042.pokemon.repository;
+import org.fit5042.pokemon.entity.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PokemonRepository extends JpaRepository<Pokemon, Long>{}
